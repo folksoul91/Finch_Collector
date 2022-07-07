c88cfc86fafd375171effb31d2ce29c8a3e44e1f
Dane Kim <mr.dane@danes-mbp.lan>
2022-07-07 19:06:56 -0400

    wip:setting up project

---
+++ main_app/views.py
@@ -0,0 +1,24 @@
+from .models import Finch
+from django.shortcuts import render
+from django.views.generic.edit import CreateView
+# Add the following import
+# from django.http import HttpResponse
+
+# Define the home view
+def home(request):
+  return render(request, 'home.html')
+
+def about(request):
+  return render(request, 'about.html')
+
+def finches_index(request):
+  finches = Finch.objects.all()
+  return render(request, 'finches/index.html', {'finches':finches})
+
+def finches_detail(request, finch_id):
+  finch = Finch.objects.get(id=finch_id)
+  return render(request, 'finches/detail.html', {'finch':finch})
+
+class FinchCreate(CreateView):
+  model = Finch
+  fields = '__all__'
